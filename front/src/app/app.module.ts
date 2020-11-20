d9163b1ebc863b56702d05048c6365ac08eb5108
ekarpp <eetu.karppinen@aalto.fi>
2020-11-20 20:52:48 +0200

    use material on frontend

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatTabsModule } from '@angular/material/tabs';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { NewClientComponent } from './new-client/new-client.component';
 import { MessageDialogComponent } from './message-dialog/message-dialog.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
@@ -20,10 +25,14 @@ import { AuthComponent } from './auth/auth.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    MatDialogModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     ReactiveFormsModule,
+
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatTabsModule
   ],
   providers: [],
